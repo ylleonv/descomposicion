15612ece9c1b1bac6aec5330b0210d61d788649e
Lorena <ylleonv@unal.edu.co>
2020-01-22 12:36:00 +0000

    third

--- src/Location.cpp
+++ src/Location.cpp
@@ -7,7 +7,7 @@ Location::Location(int xi, int yi) : x(xi), y(yi) { }
 
 //print function
 void Location::print(){
-  Rcpp::Rcout << "x = " << x << std::endl;
+  Rcpp::Rcout << "Lorena = " << x << std::endl;
   Rcpp::Rcout << "y = " << y << std::endl;
 }
 
