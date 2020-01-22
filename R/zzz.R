045006754477df94503ac57cf6938cc3f033ec30
Lorena <ylleonv@unal.edu.co>
2020-01-22 14:14:11 +0000

    4

--- R/zzz.R
+++ R/zzz.R
@@ -1,1 +1,13 @@
-loadModule("locationmodule", TRUE)
+# student.R
+
+#' student
+#'
+#' My cool package
+#'
+#' Imports
+#' @useDynLib descomposicion, .registration = TRUE
+#' @export locationmodule
+#' @import Rcpp
+"_PACKAGE"
+
+Rcpp::loadModule("locationmodule", what = TRUE)
